475950232746195c04574a0315b180abf40a1609
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-16 22:14:21 +0530

    imported FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputFieldComponent } from './input-field/input-field.component';
 import { ListComponent } from './list/list.component';
-import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputFieldComponent,
-    ListComponent,
-    ListItemComponent
+    ListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
