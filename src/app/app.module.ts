5977825482c1a451cfd285e336216bfb22ea6ff8
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-17 15:02:24 +0530

    adding new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayWithDivComponent } from './playWithDiv/playWithDiv.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayWithDivComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/playWithDiv/playWithDiv.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "app-play-with-div",
+    templateUrl: "./playWithDiv.component.html",
+    styleUrls: ["./playWithDiv.component.css"]
+})
+export class PlayWithDivComponent {
+    
+}
