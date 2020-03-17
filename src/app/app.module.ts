695bc1907e026c2bd6bbe147ce9799042e6d0210
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-17 16:30:52 +0530

    created list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlayWithDivComponent } from './playWithDiv/playWithDiv.component'
+import { PlayWithDivComponent } from './playWithDiv/playWithDiv.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayWithDivComponent
+    PlayWithDivComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
