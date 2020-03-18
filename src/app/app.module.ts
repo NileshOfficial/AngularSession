787c33e19923f0d2dbc42b7fccf8da2d1771a010
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-18 16:13:04 +0530

    added component to demonstrate lifecycle hooks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LifecycleDemoComponent } from './lifecycleDemo/lifecycleDemo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LifecycleDemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lifecycleDemo/lifecycleDemo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "app-lifecycle-demo",
+    templateUrl: "./lifecycleDemo.component.html",
+    styleUrls: ["./lifecycleDemo.component.css"]
+})
+export class LifecycleDemoComponent {
+    message: string;
+}
