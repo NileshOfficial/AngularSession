568dada556f9538ed7b285a309839e2f3135d19d
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-18 17:20:40 +0530

    question1 complte

--- src/app/lifecycleDemo/lifecycleDemo.component.ts
+++ src/app/lifecycleDemo/lifecycleDemo.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    OnChanges,
+    SimpleChanges,
+    Input,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+} from '@angular/core';
 
 @Component({
     selector: "app-lifecycle-demo",
     templateUrl: "./lifecycleDemo.component.html",
     styleUrls: ["./lifecycleDemo.component.css"]
 })
-export class LifecycleDemoComponent {
+export class LifecycleDemoComponent implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
+
     message: string;
+    @Input('message') messageFromAbove: string;
+
+    constructor() {
+        console.log("constructor called");
+    }
+
+    ngOnInit() {
+        console.log("ngOnInit called");
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        console.log("ngOnChanges called");
+    }
+
+    ngDoCheck() {
+        console.log("ngDoCheck called");
+    }
+
+    ngAfterContentInit() {
+        console.log("ngAfterContentInit called");
+    }
+
+    ngAfterContentChecked() {
+        console.log("ngAfterContentChecked called");
+    }
+
+    ngAfterViewInit() {
+        console.log("ngAfterViewInit called");
+    }
+
+    ngAfterViewChecked() {
+        console.log("ngAfterViewChecked called");
+    }
+
+    ngOnDestroy() {
+        console.log("ngOnDestroy called");
+    }
 }
