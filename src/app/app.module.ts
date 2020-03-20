e041f2473f17d5222ec302b7dd393f1e0ca4311f
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-20 09:42:14 +0530

    addded component postsList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsListComponent } from './postsList/postsList.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsListComponent,
   ],
   imports: [
     BrowserModule,
+    // HttpClient,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/postsList/postsList.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-posts-list',
+    templateUrl: './postsList.component.html',
+    styleUrls: ['./postsList.component.css']
+})
+export class PostsListComponent {
+    
+}
