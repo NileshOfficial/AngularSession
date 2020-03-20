01d0d5dc20a44fe01038d587d56d66959de6b534
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-20 10:34:15 +0530

    implemented getPosts

--- src/app/services/postsDataFetchService.ts
+++ src/app/services/postsDataFetchService.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import  { HttpClient } from '@angular/common/http';
-
+import post from '../objectTemplates/postTemplate'
 
 @Injectable({
     providedIn: 'root'
 })
 export class PostsDataFetchService {
     constructor(private http: HttpClient) {}
+
+    getPosts = () => 
+                this.http.get< Array<post> >(
+                    "https://jsonplaceholder.typicode.com/posts/1/comments"
+                );
 }
