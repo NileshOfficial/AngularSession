6921fa3c02c093924c58eb317c5ea025af623aac
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-25 21:15:15 +0530

    adding form components

---
+++ src/app/reactiveForm/reactiveForm.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-reactive-form',
+    templateUrl: './reactiveForm.component.html',
+    styleUrls: ['./reactiveForm.component.css']
+})
+export class ReactiveFormComponent {}
---
+++ src/app/templateForm/templateForm.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-template-form',
+    templateUrl: './templateForm.component.html',
+    styleUrls: ['./templateForm.component.css']
+})
+export class TemplateFormComponent {}
