cb38b9e7650168b9aba680ee16960ede8e4fce7e
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-26 15:26:24 +0530

    component setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './templateForm/templateForm.component';
+import { ReactiveFormComponent } from './reactiveForm/reactiveForm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
